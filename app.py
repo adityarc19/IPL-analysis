1334a0fc2a5e56cf68971af9e227865a57b17cef
Aditya Chakraborty <adityarc19@gmail.com>
2020-11-21 17:51:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import base64
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import sweetviz as sv
+
 
 st.title('IPL Cricket Stats (Player Rankings)')
 st.write('---')
@@ -134,10 +134,7 @@ if st.button('Comparison of teams based on Value of their players'):
     fig, ax = plt.subplots(figsize=fig_dims)
     sns.barplot(x='Value', y='Team', ax=ax, data=df)
     st.pyplot()
-
-if st.button("Generate Streamlit Report"):
-    report = sv.analyze(df_selected_team)
-    report.show_html(open_browser=True)
+    
    
 st.write('---')
 st.write("""To predict the 'Value' of players with statistics of your own, test out a live 
